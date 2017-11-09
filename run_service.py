2076bbfd7ec9bda07ac9e2bc36d3ecd097155f4e
Tim Jensen <tim.l.jensen@gmail.com>
2017-11-09 09:39:10 -0600

    Start the event loop using tornado's IOLoop facade.

--- run_service.py
+++ run_service.py
@@ -1,4 +1,4 @@
-import asyncio
+import functools
 import os
 
 import aiomysql
@@ -6,6 +6,7 @@ import aioredis
 import boto3
 import motor.motor_tornado
 import tornado.httpserver
+import tornado.ioloop
 import tornado.platform.asyncio
 import tornado.web
 
@@ -28,16 +29,17 @@ def make_app(config):
 
 def main(environ):
     tornado.platform.asyncio.AsyncIOMainLoop().install()
-    event_loop = asyncio.get_event_loop()
+    ioloop = tornado.ioloop.IOLoop.current()
 
     motor_client = motor.motor_tornado.MotorClient(environ["MONGODB_URI"])
     mongo_db = motor_client.get_default_database()
 
-    redis = event_loop.run_until_complete(aioredis.create_redis(
-        (environ["REDIS_HOST"], environ["REDIS_PORT"])))
-    mysql = event_loop.run_until_complete(aiomysql.connect(
-        host=environ["MYSQL_HOST"], port=int(environ["MYSQL_PORT"]), db=environ["MYSQL_DATABASE"],
-        user=environ["MYSQL_USER"], password=environ["MYSQL_PASSWORD"]))
+    redis = ioloop.run_sync(functools.partial(
+        aioredis.create_redis, (environ["REDIS_HOST"], environ["REDIS_PORT"])))
+    mysql = ioloop.run_sync(functools.partial(
+        aiomysql.connect, host=environ["MYSQL_HOST"], port=int(environ["MYSQL_PORT"]),
+        db=environ["MYSQL_DATABASE"], user=environ["MYSQL_USER"],
+        password=environ["MYSQL_PASSWORD"]))
 
     s3_bucket = boto3.resource("s3").Bucket(environ["AWS_S3_BUCKET"])
 
@@ -53,7 +55,7 @@ def main(environ):
     server = tornado.httpserver.HTTPServer(app)
     server.listen(int(environ["SERVER_PORT"]), "localhost")
 
-    event_loop.run_forever()
+    ioloop.start()
 
 
 if __name__ == "__main__":
--- tests/test_run_service.py
+++ tests/test_run_service.py
@@ -5,32 +5,40 @@ import run_service
 
 
 class TestRunService(unittest.TestCase):
-    @mock.patch("asyncio.get_event_loop")
+    @mock.patch("tornado.ioloop.IOLoop")
+    @mock.patch("functools.partial", autospec=True)
     @mock.patch("tornado.platform.asyncio.AsyncIOMainLoop", autospec=True)
     @mock.patch("tornado.httpserver.HTTPServer", autospec=True)
     @mock.patch("tornado.web.Application", autospec=True)
-    @mock.patch("boto3.resource")
-    @mock.patch("aiomysql.connect")
-    @mock.patch("aioredis.create_redis")
+    @mock.patch("boto3.resource", autospec=True)
+    @mock.patch("aiomysql.connect", autospec=True)
+    @mock.patch("aioredis.create_redis", autospec=True)
     @mock.patch("motor.motor_tornado.MotorClient", autospec=True)
     def test_main_starts_service(
             self, mock_motorclient_class, mock_create_redis, mock_connect, mock_resource,
             mock_application_class, mock_httpserver_class, mock_asynciomainloop_class,
-            mock_get_event_loop):
+            mock_partial, mock_ioloop_class):
 
         mock_motorclient = mock_motorclient_class.return_value
         mock_mongo_db = mock_motorclient.get_default_database.return_value
         mock_httpserver = mock_httpserver_class.return_value
         mock_asynciomainloop = mock_asynciomainloop_class.return_value
-        mock_eventloop = mock_get_event_loop.return_value
+        mock_ioloop = mock_ioloop_class.current.return_value
         mock_s3 = mock_resource.return_value
         mock_bucket = mock_s3.Bucket.return_value
 
         mock_redis = mock.Mock()
         mock_mysql = mock.Mock()
-        mock_eventloop.run_until_complete.side_effect = [
+        mock_ioloop.run_sync.side_effect = [
             mock_redis,
             mock_mysql
+        ]
+
+        mock_create_redis_partial = mock.Mock()
+        mock_connect_partial = mock.Mock()
+        mock_partial.side_effect = [
+            mock_create_redis_partial,
+            mock_connect_partial
         ]
 
         run_service.main({
@@ -49,20 +57,24 @@ class TestRunService(unittest.TestCase):
 
         mock_asynciomainloop_class.assert_called_once_with()
         mock_asynciomainloop.install.assert_called_once_with()
-        mock_get_event_loop.assert_called_once_with()
+        mock_ioloop_class.current.assert_called_once_with()
 
         mock_motorclient_class.assert_called_once_with("mongodb-uri")
 
-        mock_create_redis.assert_called_once_with(("redis-host", "redis-port"))
-        mock_connect.assert_called_once_with(
-            host="mysql-host", port=5555, db="mysql-database", user="mysql-user",
-            password="mysql-password")
         self.assertCountEqual(
             [
-                mock.call(mock_create_redis.return_value),
-                mock.call(mock_connect.return_value)
+                mock.call(mock_create_redis, ("redis-host", "redis-port")),
+                mock.call(
+                    mock_connect, host="mysql-host", port=5555, db="mysql-database",
+                    user="mysql-user", password="mysql-password")
             ],
-            mock_eventloop.run_until_complete.call_args_list)
+            mock_partial.call_args_list)
+        self.assertCountEqual(
+            [
+                mock.call(mock_create_redis_partial),
+                mock.call(mock_connect_partial)
+            ],
+            mock_ioloop.run_sync.call_args_list)
 
         mock_application_class.assert_called_once_with(
             mock.ANY,
@@ -76,4 +88,4 @@ class TestRunService(unittest.TestCase):
         mock_httpserver_class.assert_called_once_with(mock_application_class.return_value)
         mock_httpserver.listen.assert_called_once_with(8000, "localhost")
 
-        mock_eventloop.run_forever.assert_called_once_with()
+        mock_ioloop.start.assert_called_once_with()
