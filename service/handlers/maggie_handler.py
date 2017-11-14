632307a421e65420bd2f54076d1f7e0541926d71
Tim Jensen <tim.l.jensen@gmail.com>
2017-11-14 14:51:50 -0600

    Wrapped longer lines for better appearance on slides

--- service/handlers/maggie_handler.py
+++ service/handlers/maggie_handler.py
@@ -10,8 +10,10 @@ class MaggieHandler(tornado.web.RequestHandler):
         response = await client.fetch(self.settings["weather_uri"])
 
         body = json.loads(response.body)
-        condition = body["query"]["results"]["channel"]["item"]["condition"]
+        item = body["query"]["results"]["channel"]["item"]
+        condition = item["condition"]
         temp = condition["temp"]
         text = condition["text"]
 
-        self.finish(f"Currently {temp} degrees and {text} in Austin, TX")
+        self.finish(
+            f"Currently {temp} degrees and {text} in Austin, TX")
